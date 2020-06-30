7a9ad01d4a8f367873677a1aee9eed18bb3bb63f
taku tongo <tongoonat@outlook.com>
2020-06-30 18:53:34 +0200

    route history removed

--- src/router/index.js
+++ src/router/index.js
@@ -206,6 +206,6 @@ const routes = [
   }*/
 ];
 
-const router = new VueRouter({mode: "history", base: process.env.BASE_URL, routes});
+const router = new VueRouter({base: process.env.BASE_URL, routes});
 
 export default router;
