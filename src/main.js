0abcb93c950b104fd3e6ea263bbc0f7c9f306b7d
taku tongo <tongoonat@outlook.com>
2020-06-30 12:48:37 +0200

    Upload API

--- src/main.js
+++ src/main.js
@@ -6,15 +6,13 @@ import vuetify from "./plugins/vuetify";
 import axios from "axios";
 
 Vue.config.productionTip = false;
-axios.defaults.baseURL = "http://localhost:8081";
+axios.defaults.baseURL = "https://tktongo.pythonanywhere.com";
 if (localStorage.getItem("action") !== null) {
-  axios.defaults.headers.common["Authorization"] =
-    "Token " + localStorage.getItem("action");
+    axios.defaults.headers.common["Authorization"] = "Token " + localStorage.getItem("action");
 }
-
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App)
 }).$mount("#app");
